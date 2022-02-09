10642f6d65a0c29f9f508984a83aa1bf30d1347a
João Henrique Garla <jhenrique97@gmail.com>
2022-02-09 18:35:19 -0300

    project inicital commit

---
+++ feignclient/src/test/java/com/api/feignclient/FeignclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.feignclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeignclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
